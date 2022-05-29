ae2f865b24fa3df18fb3c7ee24c86146df52eb60
Renato Dantas Said <>
2022-05-29 06:49:20 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@teste-c-semaphore-consumer-producer/main.c
@@ -0,0 +1,62 @@
+#include <pthread.h>
+#include <semaphore.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define Letras 10			//itens Quatidade
+#define BufferSize 4		//buffers
+
+sem_t empty;
+sem_t full;
+int entrada = 0, saida = 0, buffer[BufferSize];
+pthread_mutex_t mutex;
+
+void *produtor(void *prod){
+    int item;
+    for(int i = 0; i < Letras;i++){
+        item = 'a'+i;
+        sem_wait(&empty);
+		sleep(1);
+        pthread_mutex_lock(&mutex);
+        buffer[entrada] = item;
+        printf("Producer %i: Insert Item %c at %i\n", *((int *)prod), buffer[entrada],entrada);
+		sleep(1);
+        entrada = (entrada+1)%BufferSize;
+        pthread_mutex_unlock(&mutex);
+        sem_post(&full);
+    }
+}
+
+void *consumidor(void *cons){
+	for(int i = 0; i < Letras; ++i){
+        sem_wait(&full);
+        pthread_mutex_lock(&mutex);
+        int item = buffer[saida];
+        printf("Consumidor %i: Remove Item %c from %d\n", *((int *)cons), item, saida);
+        saida = (saida + 1)%BufferSize;
+        pthread_mutex_unlock(&mutex);
+        sem_post(&empty);
+    }
+}
+
+
+int main(){
+    pthread_t pro, con;
+    pthread_mutex_init(&mutex, NULL);
+    sem_init(&empty, 0, BufferSize);
+    sem_init(&full, 0, 0);
+
+    pthread_create(&pro, NULL, (void *)produtor, (void *)&pro);
+    pthread_create(&con, NULL, (void *)consumidor, (void *)&con);
+
+    pthread_join(pro, NULL);
+    pthread_join(con, NULL);
+
+    pthread_mutex_destroy(&mutex);
+    sem_destroy(&empty);
+    sem_destroy(&full);
+
+    printf("\n Main done \n");
+    return 0;
+
+}
