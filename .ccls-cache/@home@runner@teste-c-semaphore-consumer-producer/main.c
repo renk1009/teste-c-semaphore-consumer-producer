e55d5ee2507dd58dc253ae511c0685f00fc19fea
Renato Dantas Said <84234402+renk1009@users.noreply.github.com>
2022-05-31 23:25:04 +0000

    Código Consertado

--- .ccls-cache/@home@runner@teste-c-semaphore-consumer-producer/main.c
+++ .ccls-cache/@home@runner@teste-c-semaphore-consumer-producer/main.c
@@ -4,45 +4,49 @@
 #include <stdio.h>
 
 #define Letras 10			//itens Quatidade
-#define BufferSize 4		//buffers
+#define BufferSize 5		//buffers
 
 sem_t empty;
 sem_t full;
 int entrada = 0, saida = 0, buffer[BufferSize];
-pthread_mutex_t mutex;
+//pthread_mutex_t mutex;
 
 void *produtor(void *prod){
     int item;
+	sem_wait(&empty);
     for(int i = 0; i < Letras;i++){
         item = 'a'+i;
-        sem_wait(&empty);
-		sleep(1);
-        pthread_mutex_lock(&mutex);
+		//sleep(1);
+        //pthread_mutex_lock(&mutex);
         buffer[entrada] = item;
         printf("Producer %i: Insert Item %c at %i\n", *((int *)prod), buffer[entrada],entrada);
-		sleep(1);
+		//sleep(1);
         entrada = (entrada+1)%BufferSize;
-        pthread_mutex_unlock(&mutex);
-        sem_post(&full);
+        //pthread_mutex_unlock(&mutex);
+        
     }
+	sem_post(&full);
 }
 
 void *consumidor(void *cons){
+	
+	sem_wait(&full);
+	printf("Buffer liberador");
 	for(int i = 0; i < Letras; ++i){
-        sem_wait(&full);
-        pthread_mutex_lock(&mutex);
+        
+        //pthread_mutex_lock(&mutex);
         int item = buffer[saida];
         printf("Consumidor %i: Remove Item %c from %d\n", *((int *)cons), item, saida);
         saida = (saida + 1)%BufferSize;
-        pthread_mutex_unlock(&mutex);
-        sem_post(&empty);
+        //pthread_mutex_unlock(&mutex);
     }
+	sem_post(&empty);
 }
 
 
 int main(){
     pthread_t pro, con;
-    pthread_mutex_init(&mutex, NULL);
+    //pthread_mutex_init(&mutex, NULL);
     sem_init(&empty, 0, BufferSize);
     sem_init(&full, 0, 0);
 
@@ -52,7 +56,7 @@ int main(){
     pthread_join(pro, NULL);
     pthread_join(con, NULL);
 
-    pthread_mutex_destroy(&mutex);
+   // pthread_mutex_destroy(&mutex);
     sem_destroy(&empty);
     sem_destroy(&full);
 
